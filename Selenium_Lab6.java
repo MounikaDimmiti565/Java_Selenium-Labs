3470e93ab45cb66e06c3339edc2a85aade183cc6
Mounika Dimmiti <mounikadimmiti1997@gmail.com>
2025-08-17 20:40:40 +0530

    Update Selenium_Lab6.java

--- Selenium_Lab6.java
+++ Selenium_Lab6.java
@@ -19,7 +19,7 @@ public class Selenium_Lab6 {
 		 Driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a ")).click();
 		 System.out.println(Driver.getCurrentUrl());
 
-		// Driver.findElement(By.linkText("Login")).click();
+	 //  Driver.findElement(By.linkText("Login")).click();
 		
 	//	Driver.findElement(By.id("input-email")).sendKeys("monn@gmail.com");
 	//	Driver.findElement(By.id("input-password")).sendKeys("admin123");		
@@ -29,23 +29,14 @@ public class Selenium_Lab6 {
         Driver.findElement(By.linkText("Monitors (2)")).click();
         WebElement show= Driver.findElement(By.id("input-limit"));
         Select sle = new Select(show);
-
-        
         sle.selectByIndex(1);
-       
         Driver.findElement(By.xpath("(//button[@data-original-title='Add to Cart'])[1]")).click();
         Thread.sleep(3000);
         Driver.findElement(By.linkText("Specification")).click();
         Assert.assertTrue(Driver.getPageSource().contains("Specification")); 
         Driver.findElement(By.xpath("//button[@data-original-title='Add to Wish List']")).click();
         Assert.assertTrue(Driver.getPageSource().contains("Success: You have added Apple Cinema 30\" to your wish list!"));
-        
-        
-
-        
-
-
-	}
-
+        }   
 }
+
 
