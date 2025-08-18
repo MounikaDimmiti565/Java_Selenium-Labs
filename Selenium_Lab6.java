3c449e7e9fb9e484f22e1ca88b92793cf40b76c6
Mounika Dimmiti <mounikadimmiti1997@gmail.com>
2025-08-18 09:04:51 +0530

    Update Selenium_Lab6.java

--- Selenium_Lab6.java
+++ Selenium_Lab6.java
@@ -36,7 +36,35 @@ public class Selenium_Lab6 {
         Assert.assertTrue(Driver.getPageSource().contains("Specification")); 
         Driver.findElement(By.xpath("//button[@data-original-title='Add to Wish List']")).click();
         Assert.assertTrue(Driver.getPageSource().contains("Success: You have added Apple Cinema 30\" to your wish list!"));
-        }   
-}
+		Driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div[2]/div[1]/button[1]")).click();
+        		System.out.println("'Success: You have added Apple Cinema 30\" to your wish list!'");
+        		Driver.findElement(By.xpath("//*[@id=\"search\"]/input")).sendKeys("Mobile");
+        		Driver.findElement(By.xpath("//*[@id=\"search\"]/span/button")).click();
+        		Driver.findElement(By.id("description")).click();
+        		Driver.findElement(By.id("button-search")).click();
+        		Driver.findElement(By.linkText("HTC Touch HD")).click();
+        		Driver.findElement(By.id("input-quantity")).clear();
+        		Driver.findElement(By.id("input-quantity")).sendKeys("3");
+        		Driver.findElement(By.xpath("//*[@id=\"button-cart\"]")).click();
+        		System.out.println("'Success: You have added HTC Touch HD to your  shopping cart!'");
+        		Thread.sleep(3000);
+        		Driver.findElement(By.id("cart-total")).click();
+        		Driver.findElement(By.xpath("//*[@id=\"cart\"]/ul/li[2]/div/p/a[1]/strong")).click();
+        		System.out.println("'Products markd with *** are not available in the desired quantity or not in stock!'");
+        		Driver.findElement(By.linkText("Checkout")).click();
+        		
+        		Thread.sleep(3000);
+        		Driver.findElement(By.linkText("My Account")).click();
+        		Driver.findElement(By.linkText("Logout")).click();
+        		System.out.println("Verify 'Account Logout' heading in process");
+        		Driver.findElement(By.linkText("Continue")).click();
+            }
+        
+
+
+	}
+
+    
+
 
 
