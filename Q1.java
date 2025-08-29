3b7566ef94f73abf1f39265c01387efe34470a36
Mounika Dimmiti <mounikadimmiti1997@gmail.com>
2025-08-29 09:13:53 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,74 @@
+package Selenium_Labs;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class Q1 {
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) {
+	  System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+	  WebDriver Driver=new ChromeDriver();
+	  Driver.get("https://www.google.com");
+	  String title=Driver.getTitle();
+	  System.out.println("The Title is:"+title);
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Before Method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("After Method");
+  }
+
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("before class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("After class");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("Before Test");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("After TEst");
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("Before Suite");
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("After Suite");
+  }
+
+}
